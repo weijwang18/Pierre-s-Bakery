0f22f3cd3e6bfc5b786bb7588f75cc361c4d7816
weijwang18@gmail.com <weijwang18@gmail.com>
2022-07-15 09:54:16 -0700

    add Bread's first good fail

---
+++ Pierre-s-Bakery.Tests/ModelTests/BreadTests.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery.Models;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class BreadTests
+  {
+    // Test methods go here
+    [TestMethod]
+    public void BreadCost_CostOneBreadNoDiscount_Int()
+    {
+      int breadQuantity = 1;
+      double breadCost = 5;
+      Bread breadOrder = new Bread(breadQuantity);
+      Assert.AreEqual(breadCost, breadOrder.BreadCost());
+    }
+  }
+}
