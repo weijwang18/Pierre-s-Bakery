4d9e456b04415d3d94fe48b9a34bad2c45e1f057
weijwang18@gmail.com <weijwang18@gmail.com>
2022-07-15 18:45:11 -0700

    WIP: Adding more UI logic

--- Pierre-s-Bakery/Program.cs
+++ Pierre-s-Bakery/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Bread.Models;
 using Pastry.Models;
 
-namespace Program.Models
+namespace Program
 {
   public class Program
   {
@@ -11,12 +11,12 @@ namespace Program.Models
     {
       Console.WriteLine("Hello! Welcome to Our Bakery.");
       Console.WriteLine("Please take a look at our menu~");
-      ViewTheMenu();
-    }
-      static void ViewTheMenu(){
       Console.WriteLine("-------------------------------");
       Console.WriteLine("Bread: $5 per loaf. Buy 2, get 1 free!");
       Console.WriteLine("Pastry: Buy 1 for $2 or 3 for $5!");
+      Order();
+    }
+      static void Order(){
       Console.WriteLine("Would you like to buy Bread or Pastry? (bread/pastry)");
       string stringAnswer = Console.ReadLine();
       string answer = stringAnswer.Replace(" ","").ToLower();
@@ -26,12 +26,48 @@ namespace Program.Models
       } else if (answer == "pastry")
       {
         BuyPastry();
-      } else
+      } 
+      else
       {
-        Console.WriteLine("Please enter bread or pastry.")
+        Console.WriteLine("Please enter bread or pastry.");
+        Order();
       }
-
-
+      static void BuyBread(){
+        Console.WriteLine("How many loaves would you like?");
+        string stringAnswer = Console.ReadLine();
+        int amount = int.Parse(stringAnswer);
+        BreadOrders newBreadOrder = new BreadOrders(amount);
+        Console.WriteLine("Your total will be $" + newBreadOrder.BreadCost() + "!");
+        Console.WriteLine("Would you like to buy some pastries? (yes/no)");
+        string selection = Console.ReadLine();
+        string answer = selection.Replace(" ","").ToLower();
+        if (answer == "yes" || answer == "y" )
+        {
+          BuyPastry();
+        }
+        else
+        {
+          Console.WriteLine("Have a nice day!");
+        }
+      }
+      static void BuyPastry(){
+        Console.WriteLine("How many pastries would you like?");
+        string stringAnswer = Console.ReadLine();
+        int amount = int.Parse(stringAnswer);
+        PastryOrders newPastryOrder = new PastryOrders(amount);
+        newPastryOrder.PastryCost();
+        Console.WriteLine("Your total will be $" + newPastryOrder.PastryCost() + "!");
+        Console.WriteLine("Would you like to buy some bread? (yes/no)");
+        string selection = Console.ReadLine();
+        string answer = selection.Replace(" ","").ToLower();
+        if (answer == "yes" || answer == "y" )
+        {
+          BuyBread();
+        }
+        else
+        {
+          Console.WriteLine("Have a nice day!");
+      }
     }
   }
-}
+}}
