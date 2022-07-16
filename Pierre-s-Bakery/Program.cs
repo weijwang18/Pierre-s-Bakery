cb77eedb985c8190ed2fc5bf02d4e60545b40952
weijwang18@gmail.com <weijwang18@gmail.com>
2022-07-15 19:36:15 -0700

    refine UI logic

--- Pierre-s-Bakery/Program.cs
+++ Pierre-s-Bakery/Program.cs
@@ -9,7 +9,7 @@ namespace Program
   {
     public static void Main()
     {
-      Console.WriteLine("Hello! Welcome to Our Bakery.");
+      Console.WriteLine("Hello! Welcome to Pierre's Bakery.");
       Console.WriteLine("Please take a look at our menu~");
       Console.WriteLine("-------------------------------");
       Console.WriteLine("Bread: $5 per loaf. Buy 2, get 1 free!");
@@ -37,17 +37,21 @@ namespace Program
         string stringAnswer = Console.ReadLine();
         int amount = int.Parse(stringAnswer);
         BreadOrders newBreadOrder = new BreadOrders(amount);
-        Console.WriteLine("Your total will be $" + newBreadOrder.BreadCost() + "!");
-        Console.WriteLine("Would you like to buy some pastries? (yes/no)");
+        if (amount <= 0)
+        {
+          Console.WriteLine("Please enter an amount greater than zero.");
+          BuyBread();
+        } else if (amount == 1) {
+          Console.WriteLine(amount + " loaf will cost $" + newBreadOrder.BreadCost() + "!");
+        } else {
+          Console.WriteLine(amount + " loaves will cost $" + newBreadOrder.BreadCost() + "!");
+        }
+        Console.WriteLine("Would you like to buy some pastries? (yes/'Enter' to leave)");
         string selection = Console.ReadLine();
         string answer = selection.Replace(" ","").ToLower();
         if (answer == "yes" || answer == "y" )
         {
           BuyPastry();
-        }
-        else
-        {
-          Console.WriteLine("Have a nice day!");
         }
       }
       static void BuyPastry(){
@@ -55,19 +59,22 @@ namespace Program
         string stringAnswer = Console.ReadLine();
         int amount = int.Parse(stringAnswer);
         PastryOrders newPastryOrder = new PastryOrders(amount);
-        newPastryOrder.PastryCost();
-        Console.WriteLine("Your total will be $" + newPastryOrder.PastryCost() + "!");
-        Console.WriteLine("Would you like to buy some bread? (yes/no)");
+        if (amount <= 0)
+        {
+          Console.WriteLine("Please enter an amount greater than zero.");
+          BuyPastry();
+        } else if (amount == 1) {
+          Console.WriteLine(amount + " pastry will cost $" + newPastryOrder.PastryCost() + "!");
+        } else {
+          Console.WriteLine(amount + " pastries will cost $" + newPastryOrder.PastryCost() + "!");
+        }
+        Console.WriteLine("Would you like to buy some bread? (yes/'Enter' to leave)");
         string selection = Console.ReadLine();
         string answer = selection.Replace(" ","").ToLower();
         if (answer == "yes" || answer == "y" )
         {
           BuyBread();
         }
-        else
-        {
-          Console.WriteLine("Have a nice day!");
-      }
     }
   }
 }}
