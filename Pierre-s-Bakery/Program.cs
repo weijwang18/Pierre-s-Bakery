57bebb3df01900a425151e917856baf4c4a4a7bc
weijwang18@gmail.com <weijwang18@gmail.com>
2022-07-28 00:37:24 -0700

    update program.cs to show the total price of bread and pastry included

--- Pierre-s-Bakery/Program.cs
+++ Pierre-s-Bakery/Program.cs
@@ -16,7 +16,7 @@ namespace Program
       Console.WriteLine("Pastry: Buy 1 for $2 or 3 for $5!");
       Order();
     }
-      static void Order(){
+      public static void Order(){
       Console.WriteLine("Would you like to buy Bread or Pastry? (bread/pastry)");
       string stringAnswer = Console.ReadLine();
       string answer = stringAnswer.Replace(" ","").ToLower();
@@ -32,49 +32,83 @@ namespace Program
         Console.WriteLine("Please enter bread or pastry.");
         Order();
       }
-      static void BuyBread(){
+      }
+
+      public static void BuyBread(){
         Console.WriteLine("How many loaves would you like?");
-        string stringAnswer = Console.ReadLine();
-        int amount = int.Parse(stringAnswer);
-        BreadOrders newBreadOrder = new BreadOrders(amount);
-        if (amount <= 0)
+        string loavesAnswer = Console.ReadLine();
+        int loavesamount = int.Parse(loavesAnswer);
+        BreadOrders newBreadOrder = new BreadOrders(loavesamount);
+        if (loavesamount <= 0)
         {
           Console.WriteLine("Please enter an amount greater than zero.");
           BuyBread();
-        } else if (amount == 1) {
-          Console.WriteLine(amount + " loaf will cost $" + newBreadOrder.BreadCost() + "!");
+        } else if (loavesamount == 1) {
+          Console.WriteLine(loavesamount + " loaf will cost $" + newBreadOrder.BreadCost() + "!");
         } else {
-          Console.WriteLine(amount + " loaves will cost $" + newBreadOrder.BreadCost() + "!");
+          Console.WriteLine(loavesamount + " loaves will cost $" + newBreadOrder.BreadCost() + "!");
         }
-        Console.WriteLine("Would you like to buy some pastries? (yes/'Enter' to leave)");
+        Console.WriteLine("Would you like to buy some pastries? (yes/no)");
         string selection = Console.ReadLine();
         string answer = selection.Replace(" ","").ToLower();
         if (answer == "yes" || answer == "y" )
         {
-          BuyPastry();
+          Console.WriteLine("How many pastries would you like?");
+          string pastriesAnswer = Console.ReadLine();
+          int pastriesamount = int.Parse(pastriesAnswer);
+          PastryOrders newPastryOrder = new PastryOrders(pastriesamount);
+          if (pastriesamount <= 0)
+          {
+            Console.WriteLine("Please enter an amount greater than zero.");
+            BuyPastry();
+          } else if (pastriesamount == 1) {
+            Console.WriteLine(pastriesamount + " pastry will cost $" + newPastryOrder.PastryCost() + "!");
+          } else {
+            Console.WriteLine(pastriesamount + " pastries will cost $" + newPastryOrder.PastryCost() + "!");
+          }
+          Console.WriteLine("Your total is" + " $" + (newPastryOrder.PastryCost() + newBreadOrder.BreadCost()) + "!");
+        } else if (answer == "no" || answer == "No") {
+          Console.WriteLine("Your total is" + " $" + newBreadOrder.BreadCost() + "!");
         }
       }
-      static void BuyPastry(){
+
+
+      public static void BuyPastry(){
         Console.WriteLine("How many pastries would you like?");
-        string stringAnswer = Console.ReadLine();
-        int amount = int.Parse(stringAnswer);
-        PastryOrders newPastryOrder = new PastryOrders(amount);
-        if (amount <= 0)
+        string pastriesAnswer = Console.ReadLine();
+        int pastriesamount = int.Parse(pastriesAnswer);
+        PastryOrders newPastryOrder = new PastryOrders(pastriesamount);
+        if (pastriesamount <= 0)
         {
           Console.WriteLine("Please enter an amount greater than zero.");
           BuyPastry();
-        } else if (amount == 1) {
-          Console.WriteLine(amount + " pastry will cost $" + newPastryOrder.PastryCost() + "!");
+        } else if (pastriesamount == 1) {
+          Console.WriteLine(pastriesamount + " pastry will cost $" + newPastryOrder.PastryCost() + "!");
         } else {
-          Console.WriteLine(amount + " pastries will cost $" + newPastryOrder.PastryCost() + "!");
+          Console.WriteLine(pastriesamount + " pastries will cost $" + newPastryOrder.PastryCost() + "!");
         }
-        Console.WriteLine("Would you like to buy some bread? (yes/'Enter' to leave)");
+        Console.WriteLine("Would you like to buy some bread? (yes/no)");
         string selection = Console.ReadLine();
         string answer = selection.Replace(" ","").ToLower();
         if (answer == "yes" || answer == "y" )
         {
-          BuyBread();
+              Console.WriteLine("How many loaves would you like?");
+          string loavesAnswer = Console.ReadLine();
+          int loavesamount = int.Parse(loavesAnswer);
+          BreadOrders newBreadOrder = new BreadOrders(loavesamount);
+          if (loavesamount <= 0)
+          {
+            Console.WriteLine("Please enter an amount greater than zero.");
+            BuyBread();
+          } else if (loavesamount == 1) {
+            Console.WriteLine(loavesamount + " loaf will cost $" + newBreadOrder.BreadCost() + "!");
+          } else {
+            Console.WriteLine(loavesamount + " loaves will cost $" + newBreadOrder.BreadCost() + "!");
+          }
+          Console.WriteLine("Your total is" + " $" + (newPastryOrder.PastryCost() + newBreadOrder.BreadCost()) + "!");
+        } else if (answer == "no" || answer == "No") {
+          Console.WriteLine("Your total is" + " $" + newPastryOrder.PastryCost() + "!");
         }
     }
   }
-}}
+}
